77c837e63bfa0d2974eb4c6f85e6a690270d9a4a
yahorzhushma <yahorzhushma@gmail.com>
2023-12-07 00:20:17 +0300

    1

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def start(message):
     markup = types.ReplyKeyboardMarkup()
     reg = types.KeyboardButton('Регистрация')
     sched = types.KeyboardButton('Расписание')
-    spkrs = types.KeyboardButton('Спискеры')
+    spkrs = types.KeyboardButton('Спикеры')
     merch = types.KeyboardButton('Мерч')
     loc = types.KeyboardButton('Как добраться?')
     markup.row(reg)
@@ -22,10 +22,11 @@ def start(message):
 def on_click(message):
     if message.text == 'Регистрация':
         bot.send_message(message.chat.id, 'https://docs.google.com/forms/d/e/1FAIpQLSdPyv7s8JTl6soLe1KCBbs9NlhE8qQVBBjBJTsQT91GTby7TQ/closedform')
+    elif message.text == "Расписание":
 
-@bot.message_handler()
-def get_user_text(message):
-    if message.text == "location":
+    elif message.text == "Спикеры":
+    elif message.text == "Мерч":
+    elif message.text == "Как добраться?":
         bot.send_location(1046586658, 53.878056, 27.490880)
 
 

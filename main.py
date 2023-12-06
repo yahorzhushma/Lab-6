f040a4e5d823b31c36ba51b55a91015fb1422676
yahorzhushma <yahorzhushma@gmail.com>
2023-12-06 13:18:17 +0300

    1

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import telebot
+from telebot import types
 
 bot = telebot.TeleBot('6786101341:AAG03-QppB_BaRggZsDC5jdrfwxtpn903RY')
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    mess = f'Привет, {message.from_user.first_name} {message.from_user.last_name}'
-    bot.send_message(message.chat.id, mess, parse_mode='')
+    markup = types.ReplyKeyboardMarkup()
+    reg = types.KeyboardButton('Регистрация')
+    sched = types.KeyboardButton('Расписание')
+    spkrs = types.KeyboardButton('Спискеры')
+    merch = types.KeyboardButton('Мерч')
+    loc = types.KeyboardButton('Как добраться?')
+    markup.row(reg)
+    markup.row(loc)
+    markup.row(sched, spkrs)
+    markup.row(merch)
+    bot.send_message(message.chat.id, 'Привет', reply_markup=markup)
+    bot.register_next_step_handler(message, on_click)
 
+def on_click(message):
+    if message.text == 'Регистрация':
+        bot.send_message(message.chat.id, 'https://docs.google.com/forms/d/e/1FAIpQLSdPyv7s8JTl6soLe1KCBbs9NlhE8qQVBBjBJTsQT91GTby7TQ/closedform')
 
 @bot.message_handler()
 def get_user_text(message):
